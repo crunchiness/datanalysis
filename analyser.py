ef27d5a38e9196f4845787d06a70e7f8d525eb1e
Ingvaras Merkys <ingvaras@gmail.com>
2016-01-10 20:41:52 +0000

    new analyser options; script for generating first graphs

--- analyser.py
+++ analyser.py
@@ -5,10 +5,11 @@ import csv
 import datetime
 
 
-def plot_cdf(input_file, home_ip, websites, filter_incoming=False, filter_outgoing=False, no_save=False, no_display=False, output_file=None):
+def plot_cdf(input_file, home_ip, websites, filter_incoming=False, filter_outgoing=False, no_save=False, no_display=False, output_file=None, use_log=False, clear=True, plot_params=None):
     lengths = []
     max_length = 0
     matching_entries = 0
+
     with open(input_file, 'rb') as csv_file:
         data_reader = csv.DictReader(csv_file, delimiter=',')
         for row in data_reader:
@@ -21,11 +22,19 @@ def plot_cdf(input_file, home_ip, websites, filter_incoming=False, filter_outgoi
                     lengths.append(int(row['len']))
 
     sorted_data = sorted(lengths)
+
+    # Remove some outliers
+    sorted_data = sorted_data[3:-3]
+
+    if use_log:
+        plt.xscale('log')
+
     yvals = np.arange(len(sorted_data)) / float(len(sorted_data))
     print matching_entries, 'matching entries'
 
-    if not no_display:
-        plt.plot(sorted_data, yvals)
+    if plot_params is None:
+        plot_params = 'r-' if use_log else 'b-'
+    plt.plot(sorted_data, yvals, plot_params)
 
     if not no_save:
         if output_file is None:
@@ -33,6 +42,13 @@ def plot_cdf(input_file, home_ip, websites, filter_incoming=False, filter_outgoi
             websites_str = ';'.join(websites)
             output_file = date + '__' + websites_str + '.svg'
         plt.savefig(output_file)
+
+    if not no_display:
+        plt.show()
+
+    if clear:
+        plt.clf()
+
 
 # timestamp,v,hl,tos,len,id,flags,off,ttl,p,sum,src,dst,opt,pad,website,source
 # 1448284130.978307,4,5,0,69,58949,0,0,46,17,64767,64.233.167.189,192.168.0.20,,,youtube.com,TIME
@@ -47,6 +63,8 @@ if __name__ == '__main__':
     group.add_argument('--outgoing', action='store_true', help='Filter outgoing packets')
     parser.add_argument('--no_save', action='store_true', help='Don\'t save graph to file')
     parser.add_argument('--no_display', action='store_true', help='Don\'t display graph')
+    parser.add_argument('--log', action='store_true', help='Use logarithmic scale')
+    parser.add_argument('--plot_params', help='Set color and line type of plot (pyplot syntax)')
     parser.add_argument('website', nargs='+', help='Website of interest')
     args = parser.parse_args()
     print 'Params', args
---
+++ analyser_script.py
@@ -0,0 +1,20 @@
+from analyser import plot_cdf
+input_file = 'dataset1/out.csv'
+home_ip = '192.168.0.20'
+website = ['youtube.com']
+no_display = True
+
+print 'youtube.com incoming traffic'
+plot_cdf(input_file, home_ip, website, filter_incoming=True, no_display=no_display, output_file='chrome_youtube.com_incoming_dataset1.svg')
+print 'youtube.com incoming traffic logarithmic'
+plot_cdf(input_file, home_ip, website, filter_incoming=True, no_display=no_display, output_file='chrome_youtube.com_incoming_log_dataset1.svg', use_log=True)
+
+
+print 'youtube.com outgoing traffic'
+plot_cdf(input_file, home_ip, website, filter_outgoing=True, no_display=no_display, output_file='chrome_youtube.com_outgoing_dataset1.svg')
+print 'youtube.com outgoing traffic logarithmic'
+plot_cdf(input_file, home_ip, website, filter_outgoing=True, no_display=no_display, output_file='chrome_youtube.com_outgoing_log_dataset1.svg', use_log=True)
+
+print 'youtube.com incoming/outgoing log combined'
+plot_cdf(input_file, home_ip, website, filter_incoming=True, no_display=no_display, no_save=True, use_log=True, plot_params='g-', clear=False)
+plot_cdf(input_file, home_ip, website, filter_outgoing=True, no_display=no_display, output_file='chrome_youtueb.com_combined_log_dataset1.svg', use_log=True, plot_params='r-')
