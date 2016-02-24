d3d96a2566827eb14101b7d92ad955f85979716a
Ingvaras Merkys <ingvaras@gmail.com>
2016-02-24 22:30:37 +0000

    now counts traffic size per second

--- packets_over_time.py
+++ packets_over_time.py
@@ -1,8 +1,11 @@
+import math
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
+from matplotlib.ticker import FuncFormatter
 
 
-def plot_thing(input_file, home_ip, plot_all=True, plot_second=False, plot_rest=False, website='youtube.com', is_down=True, colors=None, chart=None, names=None):
+def plot_thing(input_file, home_ip, plot_all=True, plot_second=False, plot_rest=False, website='youtube.com', is_down=True, colors=None, chart=None, names=None, sizes=True):
     if names is None:
         names = ['All streams', 'Top stream', 'Rest']
     if colors is None:
@@ -23,34 +26,48 @@ def plot_thing(input_file, home_ip, plot_all=True, plot_second=False, plot_rest=
                 # if row['dst'] == home_ip and row['is_ack'] == 'False':
                 # outgoing
                 if row['dst' if is_down else 'src'] == home_ip and row['is_ack'] == 'False':
-                    stream_id = row['src'] + row['src_port'] + row['dst'] + row['dst_port']
+                    stream_id = '{0}:{1}|{2}:{3}'.format(row['src'], row['src_port'], row['dst'], row['dst_port'])
                     try:
-                        streams[stream_id].append(float(row['timestamp']))
+                        streams[stream_id].append((float(row['timestamp']), int(row['len'])))
                     except KeyError:
-                        streams[stream_id] = [float(row['timestamp'])]
+                        streams[stream_id] = [(float(row['timestamp']), int(row['len']))]
 
-    sorted_streams = sorted(streams.values(), key=lambda timestamps: timestamps[0])
-    first_timestamp = sorted_streams[0][0]
-    sorted_streams = map(lambda stream: map(lambda ts: ts - first_timestamp, stream), sorted_streams)
+    # sort list of lists by first value of each list
+    sorted_streams = sorted(streams.values(), key=lambda tuples: tuples[0][0])
+    first_timestamp = sorted_streams[0][0][0]
+
+    # subtract first timestamp from all
+    sorted_streams = map(lambda stream: map(lambda tup: (tup[0] - first_timestamp, tup[1]), stream), sorted_streams)
+
+    # merge list of lists
     all_streams = reduce(lambda x, y: x + y, sorted_streams, [])
 
-    def put_in_bins(time_stamps):
+    def put_in_bins(time_stamps, size=True):
+        """
+        Puts packets into time bins (e. g. packets of same second)
+        :param time_stamps: list of tuples (time_stamp, packet_length)
+        :param size: (optional) if True, sums up lengths, otherwise number of packets
+        :return:
+        """
         time_bins = []
-        for time_stamp in time_stamps:
+        for time_stamp, length in time_stamps:
             bin_index = int(time_stamp / window)
             try:
-                time_bins[bin_index] += 1
+                time_bins[bin_index] += length if size else 1
             except IndexError:
                 while len(time_bins) != bin_index:
                     time_bins.append(0)
-                time_bins.append(1)
+                time_bins.append(length if size else 1)
         return time_bins
 
     plot_line_list = []
     plot_name_list = []
     if chart is None:
         fig, ax = plt.subplots()
-        ax.set_ylabel('# of packets')
+        if sizes:
+            ax.set_ylabel('size in KB')
+        else:
+            ax.set_ylabel('# of packets')
         ax.set_xlabel('time, s')
     else:
         fig, ax, plot_line_list, plot_name_list = chart
@@ -59,22 +76,52 @@ def plot_thing(input_file, home_ip, plot_all=True, plot_second=False, plot_rest=
     second_top_stream = reduce(lambda x, y: x + y, sorted(sorted_streams, key=lambda stream: len(stream), reverse=True)[1:], [])
 
     i = 0
+    all_values = []
     if plot_all:
-        red_line = plt.plot(put_in_bins(all_streams), colors[i] + '-')
+        values = put_in_bins(all_streams, sizes)
+        all_values.extend(values)
+        red_line = plt.plot(values, colors[i] + '-')
         plot_line_list.append(red_line[0])
         plot_name_list.append(names[i])
         i += 1
     if plot_second:
-        blue_line = plt.plot(put_in_bins(top_stream), colors[i] + '-')
+        values = put_in_bins(top_stream, sizes)
+        all_values.extend(values)
+        blue_line = plt.plot(values, colors[i] + '-')
         plot_line_list.append(blue_line[0])
         plot_name_list.append(names[i])
         i += 1
     if plot_rest:
-        green_line = plt.plot(put_in_bins(second_top_stream), colors[i] + '-')
+        values = put_in_bins(second_top_stream, sizes)
+        all_values.extend(values)
+        green_line = plt.plot(values, colors[i] + '-')
         plot_line_list.append(green_line[0])
         plot_name_list.append(names[i])
         i += 1
-    plt.legend(tuple(plot_line_list), tuple(plot_name_list))
+    plt.legend(plot_line_list, plot_name_list)
+    if sizes:
+        units = {
+            0: 'B',
+            1: 'KB',
+            2: 'MB',
+            3: 'GB',
+            4: 'TB'
+        }
+        step_size = 1
+        i = 0
+        power = 0
+        while (1.1 * max(all_values) - step_size) / step_size >= 15:
+            step_size *= 10
+            i += 1
+            if i == 3:
+                step_size = (step_size / 1000) * 1024
+                i = 0
+                power += 1
+        if (1.1 * max(all_values) - step_size) / step_size < 5:
+            step_size /= 5
+        ax.yaxis.set_major_formatter(FuncFormatter(lambda y, pos: '{0:.0f} {1}'.format(round(y / math.pow(1024, power), 0), units[power])))
+        tix = np.arange(step_size, 1.1 * max(all_values), step_size)
+        plt.yticks(tix)
     return fig, ax, plot_line_list, plot_name_list
 
 
@@ -89,7 +136,7 @@ def plot(which, show):
             'ip': '10.0.2.15'
         }
 
-    if which in 'el_manana_android_over_time_incoming':
+    if which in 'el_manana_android_over_time_incoming_1':
         plot_thing(android['file'], android['ip'], is_down=True, colors=['r'], names=['All streams'])
         plt.savefig('el_manana_android_over_time_incoming.svg')
         if show:
@@ -107,7 +154,7 @@ def plot(which, show):
         if show:
             plt.show()
         plt.clf()
-    if which in 'el_manana_chrome_over_time_incoming':
+    if which in 'el_manana_chrome_over_time_incoming_1':
         plot_thing(chrome['file'], chrome['ip'], is_down=True, colors=['r'], names=['All streams'])
         plt.savefig('el_manana_chrome_over_time_incoming.svg')
         if show:
@@ -133,8 +180,9 @@ def plot(which, show):
             plt.show()
         plt.clf()
     if which in 'el_manana_both_outgoing':
-        chart = plot_thing(chrome['file'], chrome['ip'], colors=['r'], is_down=False, names=['YouTube Chrome'])
-        plot_thing(android['file'], android['ip'], colors=['b'], is_down=False, chart=chart, names=['YouTube Android'])
+        chart = plot_thing(android['file'], android['ip'], colors=['b'], is_down=False, names=['YouTube Android'])
+        plot_thing(chrome['file'], chrome['ip'], colors=['r'], is_down=False, names=['YouTube Chrome'], chart=chart)
+
         plt.savefig('el_manana_both_outgoing.svg')
         if show:
             plt.show()
