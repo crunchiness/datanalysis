96acaaa8f4a11cb45e364cea6dfb9b1ff05f4808
Ingvaras Merkys <ingvaras@gmail.com>
2016-03-10 17:45:52 +0000

    rewriting/improving chrome_grapher

--- chrome_grapher.py
+++ chrome_grapher.py
@@ -1,65 +1,8 @@
-import argparse
 import numpy as np
-import matplotlib.pyplot as plt
 import csv
 import datetime
 
-
-def plot_cdf(input_file, home_ip, websites, filter_incoming=False, filter_outgoing=False, no_save=False, no_display=False, output_file=None, use_log=False, clear=True, ax=None, plot_params=None):
-    lengths_dict = {}
-    lengths = []
-    matching_entries = 0
-
-    with open(input_file, 'rb') as csv_file:
-        data_reader = csv.DictReader(csv_file, delimiter=',')
-        for row in data_reader:
-            if row['website'] in websites:
-                if filter_incoming and row['dst'] == home_ip or \
-                   filter_outgoing and row['src'] == home_ip:
-                    matching_entries += 1
-                    lengths.append(int(row['len']))
-                    try:
-                        lengths_dict[int(row['len'])] += 1
-                    except KeyError:
-                        lengths_dict[int(row['len'])] = 1
-
-    sorted_data = sorted(lengths)
-
-    # Remove some outliers
-    sorted_data = sorted_data[3:-3]
-    lengths_dict = remove_outliers(lengths_dict)
-
-    yvals = np.arange(len(sorted_data)) / float(len(sorted_data))
-    print matching_entries, 'matching entries'
-
-    if ax is None:
-        fig, ax = plt.subplots()
-    ax.set_yticklabels(['{:3}%'.format(x * 100) for x in ax.get_yticks()])
-    ax.set_ylabel('% Total')
-    ax.set_xlabel('Packet size in bytes')
-
-    if use_log:
-        plt.xscale('log')
-
-    if plot_params is None:
-        plot_params = 'r-' if use_log else 'b-'
-    plt.tight_layout()
-
-    plt.plot(sorted_data, yvals, plot_params)
-
-    if not no_save:
-        if output_file is None:
-            date = datetime.datetime.now().isoformat().replace(':', '_')
-            websites_str = ';'.join(websites)
-            output_file = date + '__' + websites_str + '.svg'
-        plt.savefig(output_file)
-
-    if not no_display:
-        plt.show()
-
-    if clear:
-        plt.clf()
-    return ax, lengths_dict
+import matplotlib.pyplot as plt
 
 
 def remove_outliers(lengths_dict, low_end=3, high_end=3):
@@ -79,7 +22,7 @@ def remove_outliers(lengths_dict, low_end=3, high_end=3):
     return dict(elements)
 
 
-def length_bar_chart(lengths_dict, no_save=False, no_display=False, output_file=None):
+def length_bar_chart(lengths_dict, no_save=False, show=False, output_file=None):
 
     output_list = []
 
@@ -135,27 +78,102 @@ def length_bar_chart(lengths_dict, no_save=False, no_display=False, output_file=
             output_file = datetime.datetime.now().isoformat().replace(':', '_') + '__bar_chart.svg'
         plt.savefig(output_file)
 
-    if not no_display:
+    if show:
         plt.show()
     plt.clf()
 
 
-# timestamp,v,hl,tos,len,id,flags,off,ttl,p,sum,src,dst,opt,pad,website,source
-# 1448284130.978307,4,5,0,69,58949,0,0,46,17,64767,64.233.167.189,192.168.0.20,,,youtube.com,TIME
+def get_packet_length_data(input_file, home_ip, websites, is_incoming=True, ignore_acks=True):
+    lengths_dict = {}
+    lengths = []
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(prog='ChromeGrapher', description='Produce a graph from Chrome data.')
-    parser.add_argument('input', help='Input file name of CSV')
-    parser.add_argument('ip', help='Home IP address')
-    parser.add_argument('--version', action='version', version='%(prog)s 0.1')
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--incoming', action='store_true', help='Filter incoming packets')
-    group.add_argument('--outgoing', action='store_true', help='Filter outgoing packets')
-    parser.add_argument('--no_save', action='store_true', help='Don\'t save graph to file')
-    parser.add_argument('--no_display', action='store_true', help='Don\'t display graph')
-    parser.add_argument('--log', action='store_true', help='Use logarithmic scale')
-    parser.add_argument('--plot_params', help='Set color and line type of plot (pyplot syntax)')
-    parser.add_argument('website', nargs='+', help='Website of interest')
-    args = parser.parse_args()
-    print 'Params', args
-    plot_cdf(args.input, args.ip, args.website, args.incoming, args.outgoing, args.no_save, args.no_display)
+    with open(input_file, 'rb') as csv_file:
+        data_reader = csv.DictReader(csv_file, delimiter=',')
+        for row in data_reader:
+            if ignore_acks and row['is_ack'] == 'True':
+                continue
+            if row['protocol'] != 'TCP':
+                continue
+            if websites[0] in row['website']:
+                if row['dst' if is_incoming else 'src'] == home_ip:
+                    lengths.append(int(row['len']))
+                    try:
+                        lengths_dict[int(row['len'])] += 1
+                    except KeyError:
+                        lengths_dict[int(row['len'])] = 1
+
+    # Remove some outliers
+    lengths_dict = remove_outliers(lengths_dict)
+
+    return sorted(mock_tso(lengths)), lengths_dict
+
+
+def mock_tso(data, mtu=1500):
+    new_data = []
+    for element in data:
+        full_parts = element / mtu
+        for i in xrange(element / mtu):
+            new_data.append(mtu)
+        leftover = element - full_parts * mtu
+        if leftover > 0:
+            new_data.append(element - full_parts * mtu)
+    return new_data
+
+
+def plot_packet_length(sorted_data, color='red', ax=None, use_log=False):
+    y_values = np.arange(len(sorted_data)) / float(len(sorted_data))
+    if ax is None:
+        fig, ax = plt.subplots()
+    ax.set_yticklabels(['{:3}%'.format(x * 100) for x in ax.get_yticks()])
+    ax.set_ylabel('% Total')
+    ax.set_xlabel('Packet size in bytes')
+
+    if use_log:
+        plt.xscale('log')
+
+    plt.tight_layout()
+
+    plt.plot(sorted_data, y_values, color=color)
+    return ax
+
+
+def android_in_out(show=False):
+    android_ip = '192.168.0.4'
+    website = 'youtube.com'
+    android_file = 'android_combined_dataset.csv'
+    output_file = 'android_packet_length_in_out.svg'
+
+    data_points_in, lengths_dict_in = get_packet_length_data(android_file, android_ip, website, is_incoming=True)
+    ax = plot_packet_length(data_points_in, color='red')
+
+    data_points_out, lengths_dict_out = get_packet_length_data(android_file, android_ip, website, is_incoming=False)
+    plot_packet_length(data_points_out, color='blue', ax=ax)
+    plt.savefig(output_file)
+    if show:
+        plt.show()
+
+
+def chrome_in_out(show=False):
+    chrome_ip = '10.0.2.15'
+    website = 'youtube.com'
+    # chrome_file = 'chrome_combined_dataset.csv'
+    chrome_file = 'dataset3/out.csv'
+    output_file = 'chrome_packet_length_in_out.svg'
+
+    data_points_in, lengths_dict_in = get_packet_length_data(chrome_file, chrome_ip, website, is_incoming=True)
+    data_points_in = filter(lambda x: x <= 1500, data_points_in)
+    ax = plot_packet_length(data_points_in, color='red')
+
+    data_points_out, lengths_dict_out = get_packet_length_data(chrome_file, chrome_ip, website, is_incoming=False)
+    plot_packet_length(data_points_out, color='blue', ax=ax)
+    plt.savefig(output_file)
+    if show:
+        plt.show()
+
+
+def in_chrome_android(show=False):
+    pass
+
+
+android_in_out()
+chrome_in_out()
